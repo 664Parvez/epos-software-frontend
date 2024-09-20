9d54f806ec8437f04893823c14dede6cb304ac4f
unknown <parvezrabbi664@gmail.com>
2024-09-20 10:47:08 +0600

    update

--- src/pages/Reservation.jsx
+++ src/pages/Reservation.jsx
@@ -58,7 +58,7 @@ const Reservation = () => {
                                     {
                                         items.time >= "08:00AM" &&  items.time <= "11:00AM"? 
                                         <div className="col-lg-4 col-md-6 mt-3">
-                                            <div className='p-3 rounded-3' style={{background: "var(--second-color)"}}>
+                                            <div className='p-3 rounded-3' style={{background: "var(--second-color)", borderRight: "6px solid var(--first-color)"}}>
                                                 <div className='d-flex justify-content-between align-items-center'>
                                                     <div>
                                                         <h5>Table: {items.table}</h5>
@@ -90,7 +90,7 @@ const Reservation = () => {
                                     {
                                         items.time >= "01:00PM" &&  items.time <= "03:30PM"? 
                                         <div className="col-lg-4 col-md-6 mt-3">
-                                            <div className='p-3 rounded-3' style={{background: "var(--forth-color)"}}>
+                                            <div className='p-3 rounded-3' style={{background: "var(--forth-color)", borderRight: "6px solid var(--first-color)"}}>
                                                 <div className='d-flex justify-content-between align-items-center'>
                                                     <div>
                                                         <h5>Table: {items.table}</h5>
@@ -122,7 +122,7 @@ const Reservation = () => {
                                     {
                                         items.time >= "07:00PM" &&  items.time <= "10:30PM"? 
                                         <div className="col-lg-4 col-md-6 mt-3">
-                                            <div className='p-3 rounded-3' style={{background: "var(--black-color)", color: "var(--white-color)"}}>
+                                            <div className='p-3 rounded-3' style={{background: "var(--black-color)", color: "var(--white-color)", borderRight: "6px solid var(--second-color)"}}>
                                                 <div className='d-flex justify-content-between align-items-center'>
                                                     <div>
                                                         <h5>Table: {items.table}</h5>
